fa842af3a640832dca4f35c259ca5a6179f0455b
test <test@MacBook-Pro-de-test.local>
2020-05-27 07:23:57 -0400

    initial commit

---
+++ build/precache-manifest.91aa3cfdff9bad4610e8cc9274d9c24e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02197ef109dd3ce1910d2be7e711627d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecfd329d67ecd6ae7e74",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "869e347b882b8628484b",
+    "url": "/static/js/2.753c3ed2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.753c3ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfd329d67ecd6ae7e74",
+    "url": "/static/js/main.42020cd7.chunk.js"
+  },
+  {
+    "revision": "b0c9d846bf9530968779",
+    "url": "/static/js/runtime-main.ff24cf7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
